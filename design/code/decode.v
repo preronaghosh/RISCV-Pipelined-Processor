41b08f3cfe52045d9657b31e2918795ce4673a3a
Prerona <preronaghosh1997@gmail.com>
2024-02-17 13:29:15 -0500

    removed tabs

--- design/code/decode.v
+++ design/code/decode.v
@@ -195,11 +195,11 @@ begin
                 if(inst[30] == 1'b0) // srli
                 begin
                     shamt = inst[24:20];
-				    funct7 = 7'b0000000;
+                    funct7 = 7'b0000000;
                 end
                 else if(inst[30] == 1'b1) // srai
                 begin    
-				    shamt = inst[24:20];
+                    shamt = inst[24:20];
                     funct7 = 7'b0100000;
                 end
             end 
